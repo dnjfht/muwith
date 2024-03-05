50a38d3155f501694c293965ad5f0f26be6e628a
Andrew Song <ontoro339@gmail.com>
2024-03-05 15:11:56 +0900

    Remove unnecessary types

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -1,48 +1,9 @@
-// 모든 필요한 타입을 모아둔 파일
-
-export enum AppPage {
-  HOME = '/',
-  SEARCH = '/search',
-  LOGIN = '/login',
-  PLAYLIST = '/playlist',
-  ALBUM = '/album',
-  ARTIST = '/artist',
-  TRACK = '/track',
-}
-
-export interface MusicData {
-  image: string;
-  title: string;
-  artist: string;
-  album: string;
-  date: string;
-  duration: string;
-  isLikeSong: boolean;
-}
-
-export interface ListImageType {
-  height: number;
-  url: string;
-  width: number;
-}
-
 export interface LibraryData {
   id: string;
   thumbnail: string;
   title: string;
   description: string;
   type: string;
-}
-
-export interface CurrentPlayList {
-  id: string;
-  type: string;
-  thumbnail: string;
-  title: string;
-  artist: string;
-  album: string;
-  duration: string;
-  isLikeSong: boolean;
 }
 
 export interface ListData {
